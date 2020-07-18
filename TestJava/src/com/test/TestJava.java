ee0034c53fbd45e847b531a871d33030ad243243
unknown <dharmendra.ksonu@gmail.com>
2020-07-18 22:47:51 +0530

    Commit changes

---
+++ TestJava/src/com/test/TestJava.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is just the first JAVA test program for my personal Git Hub( 18 July 2020)");
+	}
+
+}
